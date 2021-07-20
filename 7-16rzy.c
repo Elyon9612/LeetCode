841a97519d1d9ab19edd16d038203dcb5afb8157
Elyon9612 <lihongh@oregonstate.edu>
2021-07-19 21:58:48 -0700

    update 7/19

--- 7-16rzy.c
+++ 7-16rzy.c
@@ -8,7 +8,27 @@ class ValidIpWrapper {
     memcpy(content_, input, size);
   }
   // 说明：如果content_ 是一个合法的IPV4则返回 true，否则返回 false
-  bool IsAValidIpv4() const { /* 2.实现该函数（请尽量不要用库函数） */ }
+  bool IsAValidIpv4() const { /* 2.实现该函数（请尽量不要用库函数） */  
+    int index = 0;
+    int size = 0;
+    while(content_[index] != '\0'){
+        string s;
+        while(index != '.'){
+            if(!isdigit(content_[index])) return false;     // Check if the char is not '.' or digit
+
+            s += content_[index];
+            index ++;
+        }
+        if(s.length() > 1 && s[0] == '0')   return false;   // Check if there exist invalid number with start at 0
+        int number = stoi(s);                               // Transfer to numbers
+        if(number < 0 || number > 255)                      // Check if number is between 0-255
+            return false;
+        size ++;
+        index ++;
+    }
+    if(size != 4) return false;                             // check if there are only four group of numbers for IPv4
+    return true;
+ }
  private:
   char* content_ = nullptr;
 };
